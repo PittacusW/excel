8bed2e97024f1a29fb95f0fcf5c00585b603e2ba
PittacusW <5810290+PittacusW@users.noreply.github.com>
2025-03-28 21:52:35 +0000

    Fix styling

--- src/Export.php
+++ src/Export.php
@@ -2,31 +2,35 @@
 
 namespace PittacusW\Excel;
 
-use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class Export implements FromCollection, WithHeadings {
+class Export implements FromCollection, WithHeadings
+{
+    use Exportable;
 
-  use Exportable;
+    private $fileName;
 
-  private $fileName;
-  private $data;
-  private $headings;
+    private $data;
 
-  public function __construct(Collection $data, array $headings, string $fileName) {
-    $this->data     = $data;
-    $this->headings = $headings;
-    $this->fileName = ($fileName ?? "File") . ".xlsx";
-  }
+    private $headings;
 
-  public function collection() {
-    return $this->data;
-  }
+    public function __construct(Collection $data, array $headings, string $fileName)
+    {
+        $this->data = $data;
+        $this->headings = $headings;
+        $this->fileName = ($fileName ?? 'File').'.xlsx';
+    }
 
-  public function headings()
-  : array {
-    return $this->headings;
-  }
+    public function collection()
+    {
+        return $this->data;
+    }
+
+    public function headings(): array
+    {
+        return $this->headings;
+    }
 }
